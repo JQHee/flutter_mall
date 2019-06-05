712b6398fdfa2ec5d301d087b1de49e855a95e06
HJQ <v-huangjiaqian@midland.local>
2019-06-05 17:11:42 +0800

    提交

---
+++ flutter_mall/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mall/pages/index_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '百姓生活+',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: IndexPage()
+    );
+  }
+}
