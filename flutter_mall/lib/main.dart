6d5a9a0f41b9844506a5fccc3722982090c1a355
HJQ <v-huangjiaqian@midland.local>
2019-06-11 11:45:44 +0800

    提交

--- flutter_mall/lib/main.dart
+++ flutter_mall/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_mall/pages/index_page.dart';
 
 // 源码：https://github.com/shenghy/flutter_shop
 // 序列教程： https://www.cnblogs.com/wangjunwei/tag/flutter/
-// 2019-06-10 -> 20
+// 2019-06-10 -> 24
 
 void main() => runApp(MyApp());
 
